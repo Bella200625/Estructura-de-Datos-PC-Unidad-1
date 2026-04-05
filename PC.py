b9786f923d44bd523148a5adc8d4047bb0551af3
Bella200625 <bbotellom@unicartagena.edu.co>
2026-04-04 22:46:21 -0500

    MATRICES: Implementación de lógica y operaciones con matrices 3x3
    Creación: Matriz aleatoria (1-9) con ciclos anidados.
    
    Suma: Comparativa entre método tradicional (for) y optimizado (sum).
    
    Recorrido: Lectura por columnas y generación de transpuesta con zip(*).
    
    Formato: Impresión estética tipo tabla usando end=" ".
    
    Swapping: Intercambio directo de fila inicial y final.

--- PC.py
+++ PC.py
@@ -1,6 +1,6 @@
 import random
-
 # Declaramos el arreglo vacío con 10 espacios
+""" 
 enteros = [0] * 10
 
 # Usamos un for para recorrer y rellenar con valores aleatorios cada posicion
@@ -42,3 +42,70 @@ if not encontrado:
     print (f"El valor {numero} no fue encontrado")
     
 
+"""
+
+
+print ("\n --------Ahora se trabajara con matrices --------")
+
+filas = 3
+columnas = 3
+
+
+# esta matriz esta vacia solo esa rellena de puros ceros
+matriz = [[1 for j in range(columnas)] for i in range(filas)]
+
+# doble for pa recorrer tanto filas como columnas 
+for i in range(filas):
+    for j in range(columnas):
+        # random.randint(inicio, fin) genera un número entero aleatorio
+        matriz[i][j] = random.randint(1, 9) 
+
+for fila in matriz:
+    print(fila)
+
+print ("---------- SUMAA Forma larga (for) -----------")
+suma = 0
+for i in range (len(matriz)): 
+    
+    for j in range (len(matriz[0])):
+        suma += matriz [i][j]
+print (f"La suma de los elementos es {suma}")
+
+print ("---------- SUMAA Forma Corta (sum) -----------")
+sumaCorta = 0
+for fila in matriz: 
+    sumaCorta += sum (fila)
+print (f"forma rapida: {sumaCorta}")
+
+print ("-------- Matriz Transpuesta --------")
+
+filas = len(matriz)
+columnas = len(matriz[0])
+
+# El primer FOR recorre las COLUMNAS 
+for j in range(columnas):
+    print(f"--- Columna {j} ---")
+    # El segundo FOR recorre las FILAS 
+    for i in range(filas):
+        print(matriz[i][j])
+
+transpuesta = [list(fila) for fila in zip(*matriz)] 
+print(transpuesta)
+
+print("--- RECORRIDO POR COLUMNAS (TABLA) ---")
+
+
+for j in range(len(matriz[0])):
+    for i in range(len(matriz)):
+        print(matriz[i][j], end="   ")
+        
+    print()
+
+"""
+Bueno ahora vamos a intercambiar las filas con las columnas 
+"""
+print ("----- Intercambiar primera fila por la ultima -----")
+
+matriz [0], matriz [-1] = matriz [-1], matriz [0]
+for fila in matriz: 
+    print (fila)
